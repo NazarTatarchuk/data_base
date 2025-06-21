8b6371c55aa0d9989136b8cdbdac38edf564b654
NazarTatarchuk <andrijsevcenko619@gmail.com>
2025-06-21 10:52:11 +0300

    Update football_players.py

--- football_players.py
+++ football_players.py
@@ -2,22 +2,21 @@ import sqlite3
 import json
 from datetime import datetime
 
-
-
 def parse_data_safe(date_str):
     if date_str is None:
         return None
     try:
-        return datetime.strptime(date_str,"%Y-%m-%d").date()
+        return datetime.strptime(date_str, "%Y-%m-%d").date()
     except ValueError:
         return None
 
-conn =sqlite3.connect("football_players.db")
+conn = sqlite3.connect("football_players.db")
 cursor = conn.cursor()
 
-cursor.execute("PRAGMA foregn_keys =ON")
+cursor.execute("PRAGMA foreign_keys = ON")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS players(
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS players (
     player_id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT,
     last_name TEXT,
@@ -27,48 +26,54 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS players(
     nationality TEXT,
     goals INTEGER,
     assists INTEGER,
-    trophies INTEGER);""")
+    trophies INTEGER
+);
+""")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS players(
-    player_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    clubcareer_id TEXT PRIMARY KEY AUROINCREMENT,
-   club_name TEXT,
-   club_nat TEXT,
-    FOREIGN KEY(player_id) REFERECES players(player_id) ON DELETE CASCADE           
-    );
-    """)
 
-with open("-", 'r', encoding='utf-8') as f:
-    students = json.load(f)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS clubcareer (
+    clubcareer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    player_id INTEGER,
+    clubcareer_date DATE,
+    retirment_date DATE,
+    FOREIGN KEY(player_id) REFERENCES players(player_id) ON DELETE CASCADE
+);
+""")
+
+with open("C:\Users\Admin\Desktop\nazar_database\data_base-main\football_players_full.json", 'r', encoding='utf-8') as f:
+    players = json.load(f)
 
 for player in players:
     cursor.execute("""
-    INSERT INTO students(
-    first_name, last_name, date_of_birth, nationality, club, club_nat, goals, assists, trophies)
-    )
-    VALUES(?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO players (
+            first_name, last_name, date_of_birth, club, club_nat, nationality,
+            goals, assists, trophies
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     """, (
-        player['first_name']
-        player['last_name']
-        parse_data_safe(player['date_of_birth'])
-        player['nationality']
-        player['club']
-        player['club_nat']
-        player['goals']
-        player['assists']
+        player['first_name'],
+        player['last_name'],
+        parse_data_safe(player['date_of_birth']),
+        player['club'],
+        player['club_nat'],
+        player['nationality'],
+        player['goals'],
+        player['assists'],
         player['trophies']
     ))
- player_id = cursor.lastrowid
 
- cursor.execute("""
-                INSERT INTO clubcareer(player_id, clubcareer_date, retirment_date)
-                VALUES(?, ?, ?)
-                """,(
-                    player_id,
-                     parse_data_safe(player.get("clubcareer_date"))
-                     parse_data_safe(player.get("retirment_date"))
-                ))   
+    player_id = cursor.lastrowid
+
+    cursor.execute("""
+        INSERT INTO clubcareer (player_id, clubcareer_date, retirment_date)
+        VALUES (?, ?, ?)
+    """, (
+        player_id,
+        parse_data_safe(player.get("clubcareer_date")),
+        parse_data_safe(player.get("retirment_date"))
+    ))
 
 conn.commit()
 conn.close()
-print("data was sucsessfully added")
+print("Дані успішно додані!")
